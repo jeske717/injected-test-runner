b6d7c950f1f6385deed11f820eab156bda2decbf
ben jeske <jeske717@gmail.com>
2014-11-15 11:35:39 -0600

    added a shim application that gets used whenever an application is set on the test case that doesn't implement enough to get guice going.

--- android/src/androidTest/java/com/jeskeshouse/injectedtestrunner/components/TestActivity.java
+++ android/src/androidTest/java/com/jeskeshouse/injectedtestrunner/components/TestActivity.java
@@ -7,12 +7,13 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectExtra;
 
 public class TestActivity extends RoboActivity {
 
     public static final String EXTRA_NAME = "Extra";
 
-    //    @InjectExtra(value = EXTRA_NAME, optional = true)
+    @InjectExtra(value = EXTRA_NAME, optional = true)
     public String injectedExtra;
 
     @Inject
